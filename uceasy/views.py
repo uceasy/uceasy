a1371bb1492e0e47d5125fa74918bd733bcfab4a
Lucas Peres <lucasperes95@gmail.com>
2019-01-21 15:59:58 -0300

    Adding /index route besides /

--- uceasy/views.py
+++ uceasy/views.py
@@ -1,9 +1,8 @@
 from flask import render_template
-
 from uceasy import app
 
-
 @app.route('/')
+@app.route('/index')
 def index():
     app.logger.warning('sample message')
     return render_template('index.html')
