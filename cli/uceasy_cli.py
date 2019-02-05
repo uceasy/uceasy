e8b3de175662bc1f8cd0070a0d9b7c9a948237f8
Lucas Peres <lucasperes95@gmail.com>
2019-02-05 15:07:12 -0300

    Applying PEP

--- cli/uceasy_cli.py
+++ cli/uceasy_cli.py
@@ -1,18 +1,22 @@
 import click
 
+
 @click.group()
 def uceasy():
     pass
 
+
 @uceasy.command()
 def cmd1():
-    '''Command on uceasy'''
+    """Command on uceasy"""
     click.echo('uceasy cmd1')
+
 
 @uceasy.command()
 def cmd2():
-    '''Command on uceasy'''
+    """Command on uceasy"""
     click.echo('uceasy cmd2')
+
 
 if __name__ == '__main__':
     uceasy()
