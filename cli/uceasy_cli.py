b9b7c980b6ee57337e40cebca05683d41c7a0862
Lucas Peres <lucasperes95@gmail.com>
2019-02-05 14:28:05 -0300

    Adding first CLI script

---
+++ cli/uceasy_cli.py
@@ -0,0 +1,18 @@
+import click
+
+@click.group()
+def uceasy():
+    pass
+
+@uceasy.command()
+def cmd1():
+    '''Command on uceasy'''
+    click.echo('uceasy cmd1')
+
+@uceasy.command()
+def cmd2():
+    '''Command on uceasy'''
+    click.echo('uceasy cmd2')
+
+if __name__ == '__main__':
+    uceasy()
