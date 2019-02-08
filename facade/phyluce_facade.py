ec743584e414f9aca53b0f25462f78674863db16
Lucas Peres <lucasperes95@gmail.com>
2019-02-08 15:27:25 -0300

    Creating first template for the facade

---
+++ facade/phyluce_facade.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import subprocess
+import multiprocessing
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
+def run_quality_control():
+    pass
+
+
+def run_assembly():
+    pass
