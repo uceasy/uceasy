a1ee53d8c4a61a3a9a181d44082799b35ac6f5f3
Lucas Peres <lucasperes95@gmail.com>
2019-02-11 15:53:31 -0300

    Adding calling to quality_control

--- facade/phyluce_facade.py
+++ facade/phyluce_facade.py
@@ -1,5 +1,10 @@
-def run_quality_control():
-    pass
+import facade.quality_control as qc
+
+
+def run_quality_control(sheet, adapter_i5, adapter_i7):
+    adapters, tag_sequences, tag_maps, names = qc.prepare_inputs_for_template(sheet, adapter_i5, adapter_i7)
+    qc.render_conf_file(adapters, tag_sequences, tag_maps, names)
+    qc.run_illumiprocessor()
 
 
 def run_assembly():
