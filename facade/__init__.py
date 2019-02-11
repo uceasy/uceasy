a3d8d8b52ee2295e0bced7bc2261dbf7c3098812
Lucas Peres <lucasperes95@gmail.com>
2019-02-11 15:20:46 -0300

    Adding illumiprocessor args

--- facade/__init__.py
+++ facade/__init__.py
@@ -1,6 +1,11 @@
 import os
+from multiprocessing import cpu_count
 
 
 WORKENV = '../../workenv/'
 if not os.path.isdir(WORKENV):
     os.mkdir(WORKENV)
+INPUT = WORKENV + 'data/raw_fastq'
+OUTPUT = WORKENV + 'data/clean_fastq'
+CPU = str(cpu_count() - 2)
+TRIMMOMATIC = '~/miniconda3/envs/uceasy/share/trimmomatic/trimmomatic.jar'
