0fbb8dcafac5eb4d89ec9fd299a52e09d0e701fc
Lucas Peres <lucasperes95@gmail.com>
2019-02-11 15:53:00 -0300

    Moving away illumiprocessor variables

--- facade/__init__.py
+++ facade/__init__.py
@@ -6,10 +6,3 @@ from multiprocessing import cpu_count
 WORKENV = '../../workenv/'
 if not os.path.isdir(WORKENV):
     os.mkdir(WORKENV)
-
-# illumiprocessor arguments
-INPUT = WORKENV + 'data/raw_fastq'
-OUTPUT = WORKENV + 'data/clean_fastq'
-CPU = str(cpu_count() - 2)
-TRIMMOMATIC = '~/miniconda3/envs/uceasy/share/trimmomatic/trimmomatic.jar'
-CONF = WORKENV + 'illumiprocessor.conf'
