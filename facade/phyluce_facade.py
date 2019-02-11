448eba1dc713735d1cece57e6a903aab308834e5
Lucas Peres <lucasperes95@gmail.com>
2019-02-11 16:07:58 -0300

    Updating imports and callings

--- facade/phyluce_facade.py
+++ facade/phyluce_facade.py
@@ -1,10 +1,9 @@
-import facade.quality_control as qc
+from facade.quality_control import prepare_inputs_for_template, run_illumiprocessor
 
 
 def run_quality_control(sheet, adapter_i5, adapter_i7):
-    adapters, tag_sequences, tag_maps, names = qc.prepare_inputs_for_template(sheet, adapter_i5, adapter_i7)
-    qc.render_conf_file(adapters, tag_sequences, tag_maps, names)
-    qc.run_illumiprocessor()
+    adapters, tag_sequences, tag_maps, names = prepare_inputs_for_template(sheet, adapter_i5, adapter_i7)
+    return run_illumiprocessor(adapters, tag_sequences, tag_maps, names)
 
 
 def run_assembly():
