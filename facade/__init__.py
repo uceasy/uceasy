01512e02b441cd32d29828f684c223fd8cf37ab2
Lucas Peres <lucasperes95@gmail.com>
2019-02-11 15:21:55 -0300

    Adding comments

--- facade/__init__.py
+++ facade/__init__.py
@@ -2,9 +2,12 @@ import os
 from multiprocessing import cpu_count
 
 
+# folder to store phyluce outputs
 WORKENV = '../../workenv/'
 if not os.path.isdir(WORKENV):
     os.mkdir(WORKENV)
+
+# illumiprocessor arguments
 INPUT = WORKENV + 'data/raw_fastq'
 OUTPUT = WORKENV + 'data/clean_fastq'
 CPU = str(cpu_count() - 2)
