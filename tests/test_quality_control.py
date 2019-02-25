d529df11e0b3cd1ced3f87d60ab0da4f9e386ceb
Lucas Peres <lucasperes95@gmail.com>
2019-02-25 17:02:08 -0300

    Adding first unit test for qc

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,5 +1,15 @@
+import pytest
+import facade.phyluce_facade as facade
+
+
 def test_prepare_inputs_for_template():
-    assert False
+    adapter_i5 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC*ATCTCGTATGCCGTCTTCTGCTTG'
+    adapter_i7 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
+    sample_sheet = None
+
+    completed_process = facade.run_quality_control(sample_sheet, adapter_i5, adapter_i7)
+
+    assert completed_process.returncode == 0
 
 
 def test_run_illumiprocessor():
