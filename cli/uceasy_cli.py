13e3ab16a61023cab91218c2e17fb1d4c1d97c9c
Lucas Peres <lucasperes95@gmail.com>
2019-02-25 15:37:51 -0300

    Removing docstrings

--- cli/uceasy_cli.py
+++ cli/uceasy_cli.py
@@ -8,29 +8,24 @@ def uceasy():
 
 @uceasy.command()
 def run():
-    """Command on web"""
     pass
 
 
 @uceasy.command()
 def quality_control():
-    """Command on web"""
     pass
 
 
 @uceasy.command()
 def assembly():
-    """Command on web"""
     pass
 
 
 @uceasy.command()
 def uce_processing():
-    """Command on web"""
     pass
 
 
 @uceasy.command()
 def web():
-    """Command on web"""
     pass
