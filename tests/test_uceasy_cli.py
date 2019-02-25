7ddfdebc674051277f6f990e971ad61936189b43
Lucas Peres <lucasperes95@gmail.com>
2019-02-25 15:24:46 -0300

    Adding unit test templates for uceasy_cli

---
+++ tests/test_uceasy_cli.py
@@ -0,0 +1,22 @@
+import click
+from click.testing import CliRunner
+
+
+def test_run():
+    pass
+
+
+def test_web():
+    pass
+
+
+def test_quality_control():
+    pass
+
+
+def test_assembly():
+    pass
+
+
+def test_uce_processing():
+    pass
