9870604764a66d8178549bf6c5678526edf0186c
Lucas Peres <lucasperes95@gmail.com>
2019-02-25 15:32:35 -0300

    Adding CliRunne fixture

--- tests/test_uceasy_cli.py
+++ tests/test_uceasy_cli.py
@@ -1,20 +1,26 @@
-import click
+import pytest
+import cli.uceasy_cli as cli
 from click.testing import CliRunner
 
 
-def test_run():
+@pytest.fixture()
+def runner():
+    return CliRunner()
+
+
+def test_run(runner):
     pass
 
 
-def test_web():
+def test_web(runner):
     pass
 
 
-def test_quality_control():
+def test_quality_control(runner):
     pass
 
 
-def test_assembly():
+def test_assembly(runner):
     pass
 
 
