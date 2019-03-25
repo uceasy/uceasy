11266eca470035217187f838d42fcfe2e481cf34
Lucas Peres <lucasperes95@gmail.com>
2019-03-25 15:36:13 -0300

    Refactoring uceasy

--- cli/uceasy_cli.py
+++ cli/uceasy_cli.py
@@ -1,6 +1,11 @@
 import click
 
 
-@click.command()
-def uceasy():
-    pass
+@click.group(invoke_without_command=True)
+@click.argument('config')
+@click.pass_context
+def uceasy(ctx, config):
+    if ctx.invoked_subcommand is None:
+        click.echo('Web GUI')
+    else:
+        click.echo(config)
