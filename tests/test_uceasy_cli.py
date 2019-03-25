de0437172a94a3370c7f0fbbf13046945cc02b7f
Lucas Peres <lucasperes95@gmail.com>
2019-03-25 15:38:04 -0300

    Changing test names

--- tests/test_uceasy_cli.py
+++ tests/test_uceasy_cli.py
@@ -8,14 +8,14 @@ def runner():
     return CliRunner()
 
 
-def test_run(runner):
+def test_uceasy(runner):
     result = runner.invoke(cli.uceasy, ['flag'])
 
     assert result.exit_code == 0
     assert result.output == 'flag\n'
 
 
-def test_run(runner):
+def test_should_pass_when_uceasy_run_without_flags(runner):
     result = runner.invoke(cli.uceasy, [''])
 
     assert result.exit_code == 0
