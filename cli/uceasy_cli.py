b813647715f6561791e33fbc1c8dd8dc7f92c482
Caio Raposo <caioraposo94@hotmail.com>
2019-03-26 17:10:11 -0300

    Add optional entries to cli and test

--- cli/uceasy_cli.py
+++ cli/uceasy_cli.py
@@ -8,13 +8,21 @@ def uceasy():
 
 @uceasy.command()
 @click.option('--sheet', required=True)
-@click.option('--adapter_i7', '-7', required=True)
-@click.option('--adapter_i5', '-5', required=True)
+@click.option('--adapter_i7', '-i7', required=True)
+@click.option('--adapter_i5', '-i5', required=True)
 @click.option('--phred33', 'fastq_encoding', flag_value='phred33',
               required=True)
 @click.option('--phred64', 'fastq_encoding', flag_value='phred64')
-def ctl(sheet, adapter_i7, adapter_i5, fastq_encoding):
-    click.echo(f'{sheet} {adapter_i7} {adapter_i5} {fastq_encoding}')
+@click.option('--minimum-leng', type=int)
+@click.option('--r1-pattern', '-r1')
+@click.option('--r2-pattern', '-r2')
+@click.option('--no-merge', flag_value='no_merge')
+@click.option('--no-single-end', flag_value='no_single_end')
+def ctl(sheet, adapter_i7, adapter_i5, fastq_encoding, minimum_leng, r1_pattern, r2_pattern,
+        no_merge, no_single_end):
+
+    click.echo(f'{sheet} {adapter_i7} {adapter_i5} {fastq_encoding} {minimum_leng}' +
+               f'{r1_pattern} {r2_pattern} {no_merge} {no_single_end}')
 
 
 @uceasy.command()
--- tests/test_uceasy_cli.py
+++ tests/test_uceasy_cli.py
@@ -19,13 +19,23 @@ def test_should_pass_when_uceasy_web_subcommand_chosen(runner):
     assert result.output == 'Web GUI\n'
 
 
-def test_should_pass_when_running_basic_test(runner):
+def test_should_pass_when_ctl_basic_test(runner):
     result = runner.invoke(cli.uceasy, f'ctl --sheet {sample_sheet} ' +
                                        f'--adapter_i7 {adapter_i7} ' +
                                        f'--adapter_i5 {adapter_i5} ' +
-                                       f'--phred33')
+                                       f'--phred33 ')
 
     assert f'{sample_sheet} {adapter_i7} {adapter_i5}' in result.output
+
+
+def test_should_pass_when_ctl_no_merge_chosen(runner):
+    result = runner.invoke(cli.uceasy, f'ctl --sheet {sample_sheet} ' +
+                                       f'--adapter_i7 {adapter_i7} ' +
+                                       f'--adapter_i5 {adapter_i5} ' +
+                                       f'--phred33 ' +
+                                       f'--no-merge')
+
+    assert 'no_merge' in result.output
 
 
 def test_should_pass_when_ctl_phred33_chosen(runner):
