a5c0c03c96156a69211d17cd9ddda328d63981a9
Caio Raposo <caioraposo94@hotmail.com>
2019-03-28 15:15:31 -0300

    Finish script to run Trinity

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -1,16 +1,20 @@
 import pytest
-import facade.trinity as trinity
+from facade.trinity import prepare_samples_for_conf_file, run_trinity
+from facade import WORKENV
 
 
-CLEAN_FASTQ = '../../workenv/data/clean_fastq'
+CLEAN_FASTQ = '../../workenv/data/clean-fastq'
 SAMPLE_NAMES = 'alligator_mississippiensis'
+OUTPUT = WORKENV + 'data/trinity-assemblies'
 
 
 def test_prepare_samples_for_conf_file():
-    samples = trinity.prepare_samples_for_conf_file([SAMPLE_NAMES], CLEAN_FASTQ)
+    samples = prepare_samples_for_conf_file([SAMPLE_NAMES], CLEAN_FASTQ)
 
     assert samples[0] == f'{SAMPLE_NAMES}:{CLEAN_FASTQ}/{SAMPLE_NAMES}/split-adapter-quality-trimmed/'
 
 
 def test_run_assembly():
-    pass
+    completed_process = run_trinity([SAMPLE_NAMES], CLEAN_FASTQ, OUTPUT)
+
+    assert completed_process.returncode == 0
