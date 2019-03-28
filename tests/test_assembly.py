e88a6ad91a0b007975e48eb4e79073ca1c508e35
Lucas Peres <lucasperes95@gmail.com>
2019-03-28 16:43:09 -0300

    Moving samples_names to function

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -1,19 +1,11 @@
-from facade.trinity import prepare_samples_for_conf_file, run_trinity
-from facade import WORKENV
+from facade.trinity import run_trinity
+from facade import CLEAN_FASTQ, WORKENV
 
-
-CLEAN_FASTQ = '../../workenv/data/clean-fastq'
-SAMPLE_NAMES = 'alligator_mississippiensis'
-OUTPUT = WORKENV + 'data/trinity-assemblies'
-
-
-def test_prepare_samples_for_conf_file():
-    samples = prepare_samples_for_conf_file([SAMPLE_NAMES], CLEAN_FASTQ)
-
-    assert samples[0] == f'{SAMPLE_NAMES}:{CLEAN_FASTQ}/{SAMPLE_NAMES}/split-adapter-quality-trimmed/'
+OUTPUT = WORKENV + 'data/trinity_assemblies'
 
 
 def test_run_assembly():
-    completed_process = run_trinity([SAMPLE_NAMES], CLEAN_FASTQ, OUTPUT)
+    samples_names = 'alligator_mississippiensis'
+    completed_process = run_trinity([samples_names], CLEAN_FASTQ, OUTPUT)
 
     assert completed_process.returncode == 0
