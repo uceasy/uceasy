430eb12ac13064375510dd354528dbb9cae72762
Lucas Peres <lucasperes95@gmail.com>
2019-04-01 16:05:27 -0300

    Removing qc test

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,8 +1,4 @@
 def test_prepare_inputs_for_template():
-    adapter_i5 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC*ATCTCGTATGCCGTCTTCTGCTTG'
-    adapter_i7 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
-    sample_sheet = None
-
     assert False
 
 
