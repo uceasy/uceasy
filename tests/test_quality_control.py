0babd20a54175d353334273b9b865ebdaa8aec5a
Lucas Peres <lucasperes95@gmail.com>
2019-04-01 15:27:52 -0300

    Leaving template test for qc

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,14 +1,9 @@
-import phyluce_facade.phyluce_controller as facade
-
-
 def test_prepare_inputs_for_template():
     adapter_i5 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC*ATCTCGTATGCCGTCTTCTGCTTG'
     adapter_i7 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
     sample_sheet = None
 
-    completed_process = facade.run_quality_control(sample_sheet, adapter_i5, adapter_i7)
-
-    assert completed_process.returncode == 0
+    assert False
 
 
 def test_run_illumiprocessor():
