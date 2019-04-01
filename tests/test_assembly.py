219515575b0c961391e53ebb9a80b56f8e5cb75d
Lucas Peres <lucasperes95@gmail.com>
2019-04-01 15:44:58 -0300

    Removing assembly test

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -1,11 +1,2 @@
-from adapter.assembly import run_trinity
-from adapter import CLEAN_FASTQ, WORKENV
-
-OUTPUT = WORKENV + 'data/trinity_assemblies'
-
-
 def test_run_assembly():
-    samples_names = 'alligator_mississippiensis'
-    completed_process = run_trinity([samples_names], CLEAN_FASTQ, OUTPUT)
-
-    assert completed_process.returncode == 0
+    assert False
