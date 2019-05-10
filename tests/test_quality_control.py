a2fb0728beeaf520de069aaad21b3884887a1836
Caio Raposo <caioraposo94@gmail.com>
2019-05-10 15:14:57 -0300

    Add test for qulity control

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,6 +1,20 @@
-def test_prepare_inputs_for_template():
-    assert False
+import pytest
+import cli.uceasy_cli as cli
+from click.testing import CliRunner
+from controller.phyluce_facade import run_quality_control
+
+
+ADAPTER_I5 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC*ATCTCGTATGCCGTCTTCTGCTTG'
+ADAPTER_I7 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
+SAMPLE_SHEET = 'sample/alligator_sheet.csv'
+INPUT = 'sample/raw_fastq'
+
+
+@pytest.fixture()
+def runner():
+    return CliRunner()
 
 
 def test_run_illumiprocessor():
-    assert False
+    cmd = run_quality_control(INPUT, SAMPLE_SHEET, ADAPTER_I7, ADAPTER_I5)
+    assert cmd.returncode == 0
