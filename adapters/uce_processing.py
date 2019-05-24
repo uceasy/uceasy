1efbef5ff5f5f1009553103053697d2a90f36470
Caio Raposo <caioraposo94@gmail.com>
2019-05-24 19:54:58 -0300

    Declare UCE Processing functions

---
+++ adapters/uce_processing.py
@@ -0,0 +1,64 @@
+def match_contigs_to_probes():
+    """
+    Find contigs which are UCE loci and move aside those that are not.
+    """
+    pass
+
+
+def get_match_counts():
+    """
+    Create the data matrix configuration file.
+    """
+    pass
+
+
+def get_fastas_from_match_counts():
+    """
+    Get FASTA data for taxa in the taxon set.
+    """
+    pass
+
+
+def explode_get_fastas_file():
+    """
+    Explode the monolithic FASTA by taxon.
+    """
+    pass
+
+
+def seqcap_align():
+    """
+    Align the data.
+    """
+    pass
+
+
+def get_gblocks_trimmed_alignments_from_untrimmed():
+    """
+    Run gblocks trimming on the alignments.
+    """
+    pass
+
+
+def remove_locus_name_from_nexus_lines():
+    """
+    Align the data - turn off trimming and output FASTA.
+    """
+    pass
+
+
+def get_only_loci_with_min_taxa():
+    """
+    Generate final data matrices.
+    """
+    pass
+
+
+# Preparing data for RAxML and ExaML
+
+def format_nexus_files_for_raxml():
+    """
+    Build the concatenated data matrix.
+    """
+    pass
+
