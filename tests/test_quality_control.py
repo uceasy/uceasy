674af6107c1907e89281261aad52256c7c3ad370
Caio Raposo <caioraposo94@gmail.com>
2019-06-04 16:32:16 -0300

    Remove click from quality control test

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,6 +1,3 @@
-import pytest
-import cli.uceasy_cli as cli
-from click.testing import CliRunner
 from controller.phyluce_facade import run_quality_control
 
 
@@ -8,11 +5,6 @@ ADAPTER_I5 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC*ATCTCGTATGCCGTCTTCTGCTTG'
 ADAPTER_I7 = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
 SAMPLE_SHEET = 'sample/alligator_sheet.csv'
 INPUT = 'sample/raw_fastq'
-
-
-@pytest.fixture()
-def runner():
-    return CliRunner()
 
 
 def test_run_illumiprocessor():
