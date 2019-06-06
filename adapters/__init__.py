b376c89978336f593d667730a70806022d62153a
Caio Raposo <caioraposo94@gmail.com>
2019-06-06 16:53:19 -0300

    Fix search for phyluce environment

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -6,16 +6,17 @@ import os
 
 # Get environment variable UCEASY_PHYLUCE, default: phyluce
 phyluce_env = os.getenv('UCEASY_PHYLUCE', 'phyluce')
-
-# Search for phyluce environment and loads a json string
 cmd = ['conda', 'search', '--envs', '--json', phyluce_env]
 out = subprocess.check_output(cmd)
 phyluce_json = json.loads(out)
 
-if not phyluce_json[0]['location']:
+
+try:
+    PHYLUCE = phyluce_json[0]['location']
+except IndexError:
     raise IOError('Phyluce was not found, specify the conda environment with the UCEASY_PHYLUCE variable.')
 
-PHYLUCE         = phyluce_json[0]['location']
+
 TRIMMOMATIC     = PHYLUCE + '/bin/trimmomatic'
 ILLUMIPROCESSOR = PHYLUCE + '/bin/illumiprocessor'
 SCRIPT_TRINITY  = PHYLUCE + '/bin/phyluce_assembly_assemblo_trinity'
