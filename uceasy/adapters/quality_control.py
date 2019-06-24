46105c65dbf5202cde47a6dab70f072476cd7412
Lucas Peres <lucasperes95@gmail.com>
2019-06-24 17:27:51 -0300

    new directory uceasy

---
+++ uceasy/adapters/quality_control.py
@@ -0,0 +1,13 @@
+import subprocess
+from adapters import CPU, ILLUMIPROCESSOR
+
+
+def run_illumiprocessor(config, input, output):
+    cmd = [
+        ILLUMIPROCESSOR,
+        '--input', input,
+        '--output', output,
+        '--config', config,
+        '--cores', CPU,
+    ]
+    return subprocess.run(cmd, check=True)
