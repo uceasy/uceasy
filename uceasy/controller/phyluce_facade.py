cb8dd9e874c84f9da6db4bf91412fe10bce82f68
Caio Raposo <caioraposo94@gmail.com>
2019-07-01 14:05:38 -0300

    Refactore imports of phyluce_facade

--- uceasy/controller/phyluce_facade.py
+++ uceasy/controller/phyluce_facade.py
@@ -1,18 +1,24 @@
-from uceasy.adapters.assembly import run_trinity
-from uceasy.adapters.quality_control import run_illumiprocessor
-from uceasy.controller.env_manager import render_conf_file, prepare_illumiprocessor_conf, prepare_assembly_conf
+from uceasy.adapters import assembly
+from uceasy.adapters import quality_control
+from uceasy.controller import env_manager
 from uceasy.controller import CLEAN_FASTQ, TRINITY_ASSEMBLIES
 
 
 def run_quality_control(input, sheet, adapter_i7, adapter_i5):
-    config_dict = prepare_illumiprocessor_conf(sheet, adapter_i7, adapter_i5)
-    config = render_conf_file('illumiprocessor.conf', config_dict)
 
-    return run_illumiprocessor(config, input, CLEAN_FASTQ)
+    config_dict = env_manager.prepare_illumiprocessor_conf(sheet, adapter_i7, adapter_i5)
+    config = env_manager.render_conf_file('illumiprocessor.conf', config_dict)
+
+    return quality_control.run_illumiprocessor(config, input, CLEAN_FASTQ)
 
 
 def run_assembly():
-    config_dict = prepare_assembly_conf()
-    config = render_conf_file('assembly.conf', config_dict)
 
-    return run_trinity(config, TRINITY_ASSEMBLIES)
+    config_dict = env_manager.prepare_assembly_conf()
+    config = env_manager.render_conf_file('assembly.conf', config_dict)
+
+    return assembly.run_trinity(config, TRINITY_ASSEMBLIES)
+
+
+def run_uce_processing():
+    pass
