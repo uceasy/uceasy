b5a52238e18e38dd6965e734fdb777aac1820b78
Caio Raposo <caioraposo94@gmail.com>
2019-07-03 15:14:38 -0300

    Refactore cli flags

--- cli/uceasy_cli.py
+++ cli/uceasy_cli.py
@@ -1,5 +1,5 @@
 import click
-from uceasy.controller.phyluce_facade import run_quality_control, run_assembly
+from uceasy.controller import phyluce_facade
 
 
 @click.group()
@@ -9,17 +9,18 @@ def uceasy():
 
 @uceasy.command()
 @click.option('--input', required=True)
+@click.option('--output', required=True)
 @click.option('--sheet', required=True)
-@click.option('--adapter_i7', '-i7', required=True)
-@click.option('--adapter_i5', '-i5', required=True)
+@click.option('--adapter-i7', '-i7', required=True)
+@click.option('--adapter-i5', '-i5', required=True)
 @click.option('--phred33', 'fastq_encoding', flag_value='phred33', default='phred33')
 @click.option('--phred64', 'fastq_encoding', flag_value='phred64')
-@click.option('--min_len', type=int)
-@click.option('--r1_pattern', '-r1')
-@click.option('--r2_pattern', '-r2')
-@click.option('--no_merge', flag_value=True, default=False)
+@click.option('--min-len', type=int)
+@click.option('--r1-pattern', '-r1')
+@click.option('--r2-pattern', '-r2')
+@click.option('--no-merge', flag_value=True, default=False)
 @click.option('--single-end', flag_value=True, default=False)
-def ctl(input, sheet, adapter_i7, adapter_i5, fastq_encoding, min_len, r1_pattern, r2_pattern, no_merge, single_end):
+def ctl(input, output, sheet, adapter_i7, adapter_i5, fastq_encoding, min_len, r1_pattern, r2_pattern, no_merge, single_end):
     try:
         click.echo(
             f'''
@@ -38,9 +39,9 @@ def ctl(input, sheet, adapter_i7, adapter_i5, fastq_encoding, min_len, r1_patter
             Trinity
             '''
         )
-        run_quality_control(input, sheet, adapter_i7, adapter_i5)
+        phyluce_facade.run_quality_control(input, sheet, adapter_i7, adapter_i5)
         click.echo(f'Preprando para executar Trinity...')
-        run_assembly()
+        phyluce_facade.run_assembly()
     except Exception as e:
         click.echo(e)
 
