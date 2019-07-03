da6d9f9f3e4c1ff60efab6ecdee068fc5b656b07
Caio Raposo <caioraposo94@gmail.com>
2019-07-03 15:25:19 -0300

    Apply cli flags to context object

--- uceasy/context.py
+++ uceasy/context.py
@@ -1,10 +1,30 @@
 from collections import namedtuple
 
 
-Context = namedtuple('Context', [
+params = (
         'input',
+        'output',
         'sheet',
         'adapter_i5',
         'adapter_i7',
-        # TODO
-    ])
+
+        # OPTIONAL
+        'fastq_encoding',
+        'min_len',
+        'r1_pattern',
+        'r2_pattern',
+        'no_merge',
+        'single_end'
+        )
+
+defaults = (
+        None,
+        None,
+        None,
+        None,
+        None,
+        None
+        )
+
+Context = namedtuple('Context', params, defaults=defaults)
+
