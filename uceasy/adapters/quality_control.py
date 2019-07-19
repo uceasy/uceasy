b9c6767551dcdcf9e6a7edbf413cec912b49e7cc
Caio Raposo <caioraposo94@gmail.com>
2019-07-19 18:22:09 -0300

    Remove output dir before running illumiprocessor

--- uceasy/adapters/quality_control.py
+++ uceasy/adapters/quality_control.py
@@ -1,8 +1,15 @@
 import subprocess
+import os
+import shutil
 from uceasy.adapters import CPU, ILLUMIPROCESSOR
 
 
 def run_illumiprocessor(input, output, config):
+
+    # Prevent Illumiprocessor from asking to remove output dir
+    if os.path.isdir(output):
+        shutil.rmtree(output)
+
     cmd = [
         ILLUMIPROCESSOR,
         '--input', input,
