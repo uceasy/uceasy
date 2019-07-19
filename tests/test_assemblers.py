a6d98fb3bb35016f7ed328e94823ecf32846186c
Caio Raposo <caioraposo94@gmail.com>
2019-07-19 18:17:03 -0300

    Rename sample directory to testdata

--- tests/test_assemblers.py
+++ tests/test_assemblers.py
@@ -1,14 +1,17 @@
 from uceasy.adapters import assembly
+import os
 
 
-CONFIG = 'sample/assembly.conf'
+output = os.getcwd() + '/testdata/output'
+config = os.getcwd() + '/testdata/assembly.conf'
 
 
 def test_if_trinity_is_running():
-    cmd = assembly.run_trinity(CONFIG, CONTEXT.output + '/trinity')
+    cmd = assembly.run_trinity(config, output + '/trinity')
     assert cmd.returncode == 0
 
 
 def test_if_spades_is_running():
-    cmd = assembly.run_spades(CONFIG, CONTEXT.output + '/spades')
+    cmd = assembly.run_spades(config, output + '/spades')
     assert cmd.returncode == 0
+
--- tests/test_env_manager.py
+++ tests/test_env_manager.py
@@ -1,33 +1,38 @@
 from uceasy.controller import env_manager
 import pytest
-from tests import OUTPUT
 import os
+import shutil
 
 
-
-def test__conf_file_rendered():
-    output = OUTPUT + '/assembly.conf'
-    config_dict = {
-                'samples': {
-                    'sample0': f'{os.getcwd()}/sample/clean_fastq/sample0/split-adapter-quality-trimmed/'
-                    }
-            }
-
-    env_manager.render_conf_file(output, config_dict)
-
-    assert os.path.isfile(output)
+output = 'testdata/output'
 
 
 def test_create_output_dir():
-    output = OUTPUT + '/test_output'
+    if os.path.isdir(f'{os.getcwd()}/{output}'):
+        shutil.rmtree(f'{os.getcwd()}/{output}')
+
     env_manager.create_output(output)
 
     assert os.path.isdir(output)
 
 
 def test_fail_if_output_already_exists():
-    output = OUTPUT + '/test_output'
 
     with pytest.raises(IOError):
         env_manager.create_output(output)
+
+
+def test_conf_file_rendered():
+    file = output + '/assembly.conf'
+
+    config_dict = {
+                'samples': {
+                    'sample0': f'{os.getcwd()}/sample/clean_fastq/sample0/split-adapter-quality-trimmed/'
+                    }
+            }
+
+    env_manager.render_conf_file(file , config_dict)
+
+    assert os.path.isfile(file)
+
 
