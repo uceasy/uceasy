ffbd9a69bd5c2d2f20b4a9750d76367b41a9fa9e
Caio Raposo <caioraposo@protonmail.com>
2019-07-27 16:12:00 -0300

    Revert uce processor class from being abstract

--- uceasy/adapters/uce_processor.py
+++ uceasy/adapters/uce_processor.py
@@ -1,110 +1,97 @@
-import subprocess
-import abc
 from uceasy.adapters import CPU, PHYLUCE
 
 
-class UCEProcessor(metaclass=abc.ABCMeta):
+class UCEProcessor:
 
 
-    def __init__(self, output, contigs, log):
-        self.__output = output
-        self.__contigs = contigs
-        self.__log = log
-
-
-    @abc.abstractmethod
-    def run_uce_processing(self):
-        pass
-
-
-    def match_contigs_to_probes(self, probes):
+    def match_contigs_to_probes(self, output, contigs, probes):
         return [
             PHYLUCE + '/bin/phyluce_assembly_match_contigs_to_probes',
-            '--contigs', self.__contigs,
+            '--contigs', contigs,
             '--probes', probes,
-            '--output', self.__output
+            '--output', output
         ]
 
 
-    def get_match_counts(self, locus_db, taxon_list_config, taxon_group):
+    def get_match_counts(self, output, locus_db, taxon_list_config, taxon_group):
         return [
             PHYLUCE + '/bin/phyluce_assembly_get_match_counts',
             '--locus-db', locus_db,
             '--taxon-list-config', taxon_list_config,
             '--taxon-group', taxon_group,
-            '--output', self.__output
+            '--output', output
         ]
 
 
-    def get_fastas_from_match_counts(self, locus_db, match_count_output):
+    def get_fastas_from_match_counts(self, output, contigs, locus_db, match_count_output, log):
         return [
             PHYLUCE + '/bin/phyluce_assembly_get_fastas_from_match_counts',
-            '--contigs', self.__contigs,
+            '--contigs', contigs,
             '--locus-db', locus_db,
             '--match-count-output', match_count_output,
-            '--output', self.__output,
-            '--log-path', self.__log
+            '--output', output,
+            '--log-path', log
         ]
 
 
-    def explode_get_fastas_file(self, alignments):
+    def explode_get_fastas_file(self, output, alignments):
         return [
             PHYLUCE + '/bin/phyluce_assembly_explode_get_fastas_file',
             '--alignments', alignments,
-            '--output', self.__output,
+            '--output', output,
             '--by-taxon'
         ]
 
-    def seqcap_align(self, fasta, taxa, aligner, no_trim=False):
+    def seqcap_align(self, output, fasta, taxa, aligner, no_trim=False):
         cmd = [
             PHYLUCE + '/bin/phyluce_align_seqcap_align',
             '--fasta', fasta,
             '--taxa', taxa,
             '--aligner', aligner,
-            '--output', self.__output
+            '--output', output
         ]
         if no_trim:
             cmd.append('--no-trim')
         return cmd
 
 
-    def get_gblocks_trimmed_alignments_from_untrimmed(self, alignments):
+    def get_gblocks_trimmed_alignments_from_untrimmed(self, output, alignments, log):
         return [
             PHYLUCE + '/bin/get_gblocks_trimmed_alignments_from_untrimmed',
             '--alignments', alignments,
-            '--output', self.__output,
-            '--log', self.__log
+            '--output', output,
+            '--log', log
             ]
 
 
-    def remove_locus_name_from_nexus_lines(self, alignments):
+    def remove_locus_name_from_nexus_lines(self, output, alignments, log):
         return [
             PHYLUCE + '/bin/remove_locus_name_from_nexus_lines',
             '--alignments', alignments,
-            '--output', self.__output,
-            '--log-path', self.__log,
+            '--output', output,
+            '--log-path', log,
             '--cores', CPU
         ]
 
 
-    def get_only_loci_with_min_taxa(self, alignments, taxa, percent):
+    def get_only_loci_with_min_taxa(self, output, alignments, taxa, percent, log):
         return [
             PHYLUCE + '/bin/get_only_loci_with_min_taxa',
             '--alignments', alignments,
             '--taxa', taxa,
             '--percent', percent,
-            '--output', self.__output,
+            '--output', output,
             '--cores', CPU,
-            '--log-path', self.__log
+            '--log-path', log
         ]
 
 
-    def format_nexus_files_for_raxml(self, alignments, charsets=False):
+    def format_nexus_files_for_raxml(self, output, alignments, log, charsets=False):
         cmd = [
             PHYLUCE + '/bin/format_nexus_files_for_raxml',
             '--alignments', alignments,
-            '--output', self.__output,
-            '--log-path', self.__log
+            '--output', output,
+            '--log-path', log
         ]
         if charsets:
             cmd.append('--charsets')
