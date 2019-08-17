e56e4a6c36c8035df3bf2801fd12cdee7afb73aa
Caio Raposo <caioraposo@protonmail.com>
2019-08-16 23:06:15 -0300

    Add all steps to interactor

--- uceasy/interactor.py
+++ uceasy/interactor.py
@@ -1,4 +1,5 @@
-from uceasy.controller import facade
+from uceasy.controller.phyluce_facade import Facade
+from uceasy.controller import env_manager
 
 
 class Interactor:
@@ -6,7 +7,30 @@ class Interactor:
 
     def __init__(self, context):
         self.__context = context
+        self.__facade = Facade()
 
 
     def run(self):
-        pass
+
+        samples = env_manager.get_samples_from_csv(self.__context.sheet)
+
+        self.__facade.quality_control(self.__context.input,
+                                      self.__context.output,
+                                      self.__context.sheet,
+                                      self.__context.adapter_i7,
+                                      self.__context.adapter_i5)
+
+        self.__facade.assembly(self.__context.output, self.__context.assembler, samples)
+        
+        contigs = self.__context.output + '/assembly/contigs'
+
+        self.__facade.process_uce(self.__context.output,
+                                  self.__context.output,
+                                  contigs,
+                                  self.__context.probes,
+                                  samples,
+                                  self.__context.aligner,
+                                  self.__context.charsets,
+                                  self.__context.percent,
+                                  self.__context.internal_trimming)
+
