2a7cc4dc5fa02ecc9dde850330491a3e9e85d9bd
Caio Raposo <caioraposo@protonmail.com>
2019-10-01 08:35:07 +0000

    Rename config render functions

--- tests/unit/test_env_manager.py
+++ tests/unit/test_env_manager.py
@@ -7,15 +7,18 @@ import pandas as pd
 from uceasy.controller import env_manager
 
 
-OUTPUT = 'testoutput/env_manager_test'
-DATA = 'testdata'
-SAMPLES = ['alligator_mississippiensis_GGAGCTATGG', 'anolis_carolinensis_GGCGAAGGTT']
-SHEET = 'testdata/sample_sheet.csv'
+OUTPUT = "testoutput/env_manager_test"
+DATA = "testdata"
+SAMPLES = [
+    "alligator_mississippiensis_GGAGCTATGG",
+    "anolis_carolinensis_GGCGAAGGTT",
+]
+SHEET = "testdata/sample_sheet.csv"
 
 
 def test_create_output_dir():
-    if os.path.isdir(f'{os.getcwd()}/{OUTPUT}'):
-        shutil.rmtree(f'{os.getcwd()}/{OUTPUT}')
+    if os.path.isdir(f"{os.getcwd()}/{OUTPUT}"):
+        shutil.rmtree(f"{os.getcwd()}/{OUTPUT}")
 
     env_manager.create_output(OUTPUT)
 
@@ -28,22 +31,23 @@ def test_fail_if_output_already_exists():
         env_manager.create_output(OUTPUT)
 
 
-def test_conf_file_rendered():
-    file = OUTPUT + '/assembly.conf'
+def test_config_file_rendered():
+    file = OUTPUT + "/assembly.conf"
 
     config_dict = {
-                'samples': {
-                    'sample0': f'{os.getcwd()}/sample/clean_fastq/sample0/split-adapter-quality-trimmed/'
-                    }
-            }
+        "samples": {
+            "sample0": f"{os.getcwd()}/sample/clean_fastq/sample0/split-adapter-quality-trimmed/"
+        }
+    }
 
-    env_manager.render_conf_file(file , config_dict)
+    env_manager.render_config_file(file, config_dict)
 
     assert os.path.isfile(file)
 
 
 def test_get_samples_names():
-    sheet_samples = env_manager.get_samples_from_csv(DATA + '/sample_sheet.csv') 
+    sheet_samples = env_manager.get_samples_from_csv(
+        DATA + "/sample_sheet.csv"
+    )
 
     assert SAMPLES == sheet_samples
-
--- uceasy/controller/env_manager.py
+++ uceasy/controller/env_manager.py
@@ -3,81 +3,89 @@ import os
 import configparser
 
 
-def render_conf_file(file, config_dict):
-    config = configparser.ConfigParser(delimiters=(':'))
+def render_config_file(file, config_dict):
+    config = configparser.ConfigParser(delimiters=(":"))
     config.optionxform = str
     config.read_dict(config_dict)
 
-    with open(file, 'w') as fl:
+    with open(file, "w") as fl:
         config.write(fl, space_around_delimiters=False)
 
     return file
 
 
-def prepare_illumiprocessor_conf(sheet, adapter_i7, adapter_i5):
+def prepare_illumiprocessor_config(sheet, adapter_i7, adapter_i5):
     config_dict = dict()
     sheet = pd.read_csv(sheet)
     sheet = sheet[sheet.columns[1:4]]
 
-    sheet['i5_Tag'] = pd.Series([
-        f"sample{index}_barcode_i5"
-        for index in sheet.index.values
-    ])
-    sheet['i7_Tag'] = pd.Series([
-        f"sample{index}_barcode_i7"
-        for index in sheet.index.values
-    ])
+    sheet["i5_Tag"] = pd.Series(
+        [f"sample{index}_barcode_i5" for index in sheet.index.values]
+    )
+    sheet["i7_Tag"] = pd.Series(
+        [f"sample{index}_barcode_i7" for index in sheet.index.values]
+    )
 
-    config_dict['adapters'] = {"i7": adapter_i7, "i5": adapter_i5}
+    config_dict["adapters"] = {"i7": adapter_i7, "i5": adapter_i5}
 
-    tags_i5 = {row['i5_Tag']: row['i5_Barcode_Seq']
-               for _, row in sheet.iterrows()}
+    tags_i5 = {
+        row["i5_Tag"]: row["i5_Barcode_Seq"] for _, row in sheet.iterrows()
+    }
 
-    tags_i7 = {row['i7_Tag']: row['i7_Barcode_Seq']
-               for _, row in sheet.iterrows()}
+    tags_i7 = {
+        row["i7_Tag"]: row["i7_Barcode_Seq"] for _, row in sheet.iterrows()
+    }
 
-    config_dict['tag sequences'] = {**tags_i5, **tags_i7}
+    config_dict["tag sequences"] = {**tags_i5, **tags_i7}
 
-    config_dict['tag map'] = {row['Customer_Code']: f"{row['i5_Tag']},{row['i7_Tag']}"
-                              for _, row in sheet.iterrows()}
+    config_dict["tag map"] = {
+        row["Customer_Code"]: f"{row['i5_Tag']},{row['i7_Tag']}"
+        for _, row in sheet.iterrows()
+    }
 
-    config_dict['names'] = {row['Customer_Code']: row['Customer_Code']
-                            for _, row in sheet.iterrows()}
+    config_dict["names"] = {
+        row["Customer_Code"]: row["Customer_Code"]
+        for _, row in sheet.iterrows()
+    }
     return config_dict
 
 
-def prepare_assembly_conf(output, samples):
+def prepare_assembly_config(output, samples):
     config_dict = dict()
-    config_dict['samples'] = {sample: f'{output}/clean_fastq/{sample}/split-adapter-quality-trimmed/'
-                              for sample in samples}
+    config_dict["samples"] = {
+        sample: f"{output}/clean_fastq/{sample}/split-adapter-quality-trimmed/"
+        for sample in samples
+    }
     return config_dict
 
 
 def create_output(output):
-    output = f'{os.getcwd()}/{output}'
+    output = f"{os.getcwd()}/{output}"
     if os.path.isdir(output):
-        raise IOError('Error: output directory already exists!\n'
-                      'Rename or remove it before running UCEasy.')
+        raise IOError(
+            "Error: output directory already exists!\n"
+            "Rename or remove it before running UCEasy."
+        )
     os.mkdir(output)
 
 
 def get_samples_from_csv(sheet):
     sheet = pd.read_csv(sheet)
-    samples = [row['Customer_Code'] for _, row in sheet.iterrows()]
+    samples = [row["Customer_Code"] for _, row in sheet.iterrows()]
 
     return samples
 
 
-def render_taxon_set_conf(file, samples):
-    string = '[all]'
+def render_taxon_set_config(file, samples):
+    string = "[all]"
     for sample in samples:
-        string += f'\n{sample}'
+        string += f"\n{sample}"
 
     config = configparser.ConfigParser(allow_no_value=True)
     config.optionxform = str
     config.read_string(string)
-    
-    with open(file, 'w') as fl:
+
+    with open(file, "w") as fl:
         config.write(fl, space_around_delimiters=False)
 
     return file
