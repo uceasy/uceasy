050d3425fe67961d566ca52883e6fa219bbda847
Caio Raposo <caioraposo@protonmail.com>
2020-01-30 15:43:36 +0000

    Add ioutils module

---
+++ uceasy/ioutils.py
@@ -0,0 +1,34 @@
+import subprocess
+import configparser
+import csv
+
+
+def run_command(cmd, *args, **kargs):
+    cmd = [cmd]
+
+    for arg in args:
+        cmd.append(arg)
+    for k, v in kargs.items():
+        cmd.append(f"--{k}")
+        cmd.append(v)
+
+    return subprocess.run(cmd, capture_output=True)
+
+
+def load_csv(path, delimiter=","):
+    with open(path, "r") as f:
+        reader = csv.reader(f, delimiter=delimiter)
+
+        # Ignore header
+        next(reader)
+
+        return [row for row in reader]
+
+
+def dump_config_file(file, config_dict, allow_no_value=False):
+    config = configparser.ConfigParser(delimiters=(":"), allow_no_value=allow_no_value)
+    config.optionxform = str
+    config.read_dict(config_dict)
+
+    with open(file, "w") as fl:
+        config.write(fl, space_around_delimiters=False)
