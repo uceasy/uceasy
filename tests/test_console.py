925c7feebe3a04e855034eb8416eaae32bdc3aa1
Caio Raposo <caioraposo@protonmail.com>
2020-03-05 15:13:15 +0000

    Add simple cli test

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+import click.testing
+import pytest
+
+from uceasy import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
