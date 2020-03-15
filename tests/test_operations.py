24662298f25be15b32f6be829c3e41677cbdca57
Caio Raposo <caioraposo@protonmail.com>
2020-03-15 19:30:40 +0000

    Add single indexed test cases

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -23,27 +23,48 @@ def config():
     return config
 
 
+@pytest.fixture
+def config_single_index():
+    csv_file = load_csv(CSV)
+    config = parse_illumiprocessor_config(csv_file, double_index=False)
+    return config
+
+
 def test_illumiprocessor_config_dict_has_adapters(config):
-    assert "adapters" in config
     assert config["adapters"]["i7"] == I7_ADAPTER
     assert config["adapters"]["i5"] == I5_ADAPTER
 
 
 def test_illumiprocessor_config_has_tag_sequences(config):
-    assert "tag sequences" in config
     assert config["tag sequences"]["sample0_barcode_i7"] == SAMPLE0_BARCODE_I7
     assert config["tag sequences"]["sample0_barcode_i5"] == SAMPLE0_BARCODE_I5
     assert config["tag sequences"]["sample1_barcode_i7"] == SAMPLE1_BARCODE_I7
     assert config["tag sequences"]["sample1_barcode_i5"] == SAMPLE1_BARCODE_I5
 
 
+def test_illumiprocessor_config_single_index_tag_sequences(config_single_index):
+    assert (
+        config_single_index["tag sequences"]["sample0_barcode_i7"]
+        == SAMPLE0_BARCODE_I7
+    )
+    assert (
+        config_single_index["tag sequences"]["sample1_barcode_i7"]
+        == SAMPLE1_BARCODE_I7
+    )
+    assert "sample0_barcode_i5" not in config_single_index["tag sequences"]
+    assert "sample1_barcode_i5" not in config_single_index["tag sequences"]
+
+
 def test_illumiprocessor_config_has_tag_map(config):
-    assert "tag map" in config
     assert config["tag map"][SAMPLE0] == "sample0_barcode_i7,sample0_barcode_i5"
     assert config["tag map"][SAMPLE1] == "sample1_barcode_i7,sample1_barcode_i5"
 
 
+def test_illumiprocessor_config_single_index_tag_map(config_single_index):
+    assert config_single_index["tag map"][SAMPLE0] == "sample0_barcode_i7"
+    assert config_single_index["tag map"][SAMPLE1] == "sample1_barcode_i7"
+
+
 def test_illumiprocessor_config_has_names(config):
-    assert "names" in config
     assert config["names"][SAMPLE0] == SAMPLE0.lower()
     assert config["names"][SAMPLE1] == SAMPLE1.lower()
