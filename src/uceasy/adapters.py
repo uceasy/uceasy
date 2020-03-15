317ca2adffd05515e9bfdb209f0ca3adc41d12a4
Caio Raposo <caioraposo@protonmail.com>
2020-03-15 16:29:56 +0000

    Add trinity adapter and generic runner

--- src/uceasy/adapters.py
+++ src/uceasy/adapters.py
@@ -10,11 +10,37 @@ def adapter(func):
     return func
 
 
+def _run_adapter(
+    cmd: List[str], capture_output: bool, dir_to_execute: str
+) -> List[str]:
+    """
+    Utilitary runner to be used by the adapters.
+
+    :param cmd              the command passed by the adapter as a list.
+    :param capture_output   True if you want to receive the output of the
+                            command.
+    :param dir_to_execute   execute in a different directory.
+    :return:                empty list in case of "capture_output=False",
+                            otherwise the lines of the command output as a
+                            list. One line of output is one item in the list.
+    """
+    kwargs = {"cwd": dir_to_execute}
+
+    if capture_output:
+        kwargs.update(stdout=PIPE, stderr=DEVNULL, universal_newlines=True)
+
+    cmd_return = run(cmd, **kwargs)
+
+    if capture_output:
+        return cmd_return.stdout.strip().splitlines()
+    return []
+
+
 @adapter
 def illumiprocessor(
     args_as_list: List[str],
     dir_to_execute: str = None,
-    capture_output: str = False,
+    capture_output: bool = False,
 ) -> List[str]:
     """
     illumiprocessor wrapper. see: https://illumiprocessor.readthedocs.io/en/
@@ -29,15 +55,27 @@ def illumiprocessor(
                             otherwise the lines of illumiprocessor output as a
                             list. One line of output is one item in the list.
     """
-    cmd = ["illumiprocessor"]
-    cmd += args_as_list
-    kwargs = {"cwd": dir_to_execute}
+    cmd = ["illumiprocessor"] + args_as_list
+    return _run_adapter(cmd, capture_output, dir_to_execute)
 
-    if capture_output:
-        kwargs.update(stdout=PIPE, stderr=DEVNULL, universal_newlines=True)
 
-    illumiprocessor_return = run(cmd, **kwargs)
+@adapter
+def trinity(
+    args_as_list: List[str],
+    dir_to_execute: str = None,
+    capture_output: bool = False,
+) -> List[str]:
+    """
+    phyluce's trinity wrapper.
+    provide the trinity arguments as a list via "args_as_list".
+    e.g. ["--contig", "clean-fastq", "--cores", "4"]
 
-    if capture_output:
-        return illumiprocessor_return.stdout.strip().splitlines()
-    return []
+    :param args_as_list:    list of arguments to trinity.
+    :param capture_output:  True if you want to receive the output of
+                            trinity.
+    :return:                empty list in case of "capture_output=False",
+                            otherwise the lines of trinity output as a
+                            list. One line of output is one item in the list.
+    """
+    cmd = ["phyluce_assembly_assemblo_trinity", "--clean"] + args_as_list
+    return _run_adapter(cmd, capture_output, dir_to_execute)
--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,6 +1,11 @@
 from uceasy.adapters import ADAPTERS
 
 
-def test_illumiprocessor():
+def test_illumiprocessor_help():
     cmd = ADAPTERS["illumiprocessor"](["--help"], capture_output=True)
     assert "usage: illumiprocessor" in cmd[0]
+
+
+def test_trinity_help():
+    cmd = ADAPTERS["trinity"](["--help"], capture_output=True)
+    assert "usage: phyluce_assembly_assemblo_trinity" in cmd[0]
