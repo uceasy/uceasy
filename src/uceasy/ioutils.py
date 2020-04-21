5715258fb910d3c148711f968907c6dfe198dbf8
Caio Raposo <caioraposo@protonmail.com>
2020-04-21 18:00:49 -0300

    Allow the config object to be passed as string and add get_taxa_from_contigs

--- src/uceasy/ioutils.py
+++ src/uceasy/ioutils.py
@@ -1,5 +1,6 @@
 import configparser
 import csv
+import os
 from typing import List
 
 
@@ -15,14 +16,22 @@ def load_csv(path: str, delimiter: str = ",") -> List[str]:
 
 
 def dump_config_file(
-    path: str, config: dict, allow_no_value: bool = False
+    path: str, config, allow_no_value: bool = False, from_string: bool = False,
 ) -> None:
     """Read a dictionary and create a .ini style configuration file."""
     parser = configparser.ConfigParser(
         delimiters=(":"), allow_no_value=allow_no_value
     )
     parser.optionxform = str
-    parser.read_dict(config)
+    if from_string:
+        parser.read_string(config)
+    else:
+        parser.read_dict(config)
 
     with open(path, "w") as fl:
         parser.write(fl, space_around_delimiters=False)
+
+
+def get_taxa_from_contigs(contigs: str) -> int:
+    """Return the taxa number from the contigs directory."""
+    return len([name for name in os.listdir(contigs) if os.path.isfile(name)])
