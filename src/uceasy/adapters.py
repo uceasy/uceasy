4b84fab63d7f2cdc33fa04e149cb064a8923ba4e
Caio Raposo <caioraposo@protonmail.com>
2020-06-25 15:02:40 -0300

    Remove unnecessary braces

--- src/uceasy/adapters.py
+++ src/uceasy/adapters.py
@@ -15,11 +15,11 @@ TEMPLATES = {
     "spades": "phyluce_assembly_assemblo_spades",
     "match_contigs_to_probes": "phyluce_assembly_match_contigs_to_probes",
     "get_match_counts": "phyluce_assembly_get_match_counts",
-    "get_fastas_from_match_counts": ("phyluce_assembly_get_fastas_from_match_counts"),
+    "get_fastas_from_match_counts": "phyluce_assembly_get_fastas_from_match_counts",
     "explode_get_fastas_file": "phyluce_assembly_explode_get_fastas_file",
     "secap_align": "phyluce_align_seqcap_align",
     "gblocks": "phyluce_align_get_gblocks_trimmed_alignments_from_untrimmed",
-    "remove_locus_name_from_nexus_lines": ("phyluce_align_remove_locus_name_from_nexus_lines"),
+    "remove_locus_name_from_nexus_lines": "phyluce_align_remove_locus_name_from_nexus_lines",
     "get_only_loci_with_min_taxa": "phyluce_align_get_only_loci_with_min_taxa",
     "nexus_to_raxml": "phyluce_align_format_nexus_files_for_raxml",
 }
