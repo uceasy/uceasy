e300ec99193d1eb4b1418bf71a2c2df1dbbfba8f
Caio Raposo <caioraposo@protonmail.com>
2021-08-14 17:24:10 -0300

    Move provenance tracking logic from run.py to tracking.py

--- src/uceasy/run.py
+++ src/uceasy/run.py
@@ -1,47 +1,16 @@
-import sys
-import platform
-import getpass
 from types import SimpleNamespace
-from datetime import datetime
-from time import time, gmtime, strftime
 
 from uceasy.facade import AssemblyFacade, QualityControlFacade, UCEPhylogenomicsFacade
 from uceasy.tracking import save_tracking_file
 
 
-tracking = {
-    "Operating System": platform.platform(),
-    "Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "UCEasy command": "uceasy " + " ".join(sys.argv[1:]),
-    "Who ran": getpass.getuser(),
-}
-
-
 def run_quality_control(context: SimpleNamespace):
-    start_time = time()
-    out = QualityControlFacade(context).run()
-    tracking["Execution Time"] = strftime("%H:%M:%S", gmtime(time() - start_time))
-    tracking["FASTQ"] = context.raw_fastq
-    tracking["Standard output"] = out
-    tracking["Commands"] = context.commands
-    save_tracking_file(context.log_dir + "/" + context.tracking_file, tracking)
+    save_tracking_file(QualityControlFacade(context).run())
 
 
 def run_assembly(context: SimpleNamespace):
-    start_time = time()
-    out = AssemblyFacade(context).run()
-    tracking["Execution Time"] = strftime("%H:%M:%S", gmtime(time() - start_time))
-    tracking["FASTQ"] = context.clean_fastq
-    tracking["Standard output"] = out
-    tracking["Commands"] = context.commands
-    save_tracking_file(context.log_dir + "/" + context.tracking_file, tracking)
+    save_tracking_file(AssemblyFacade(context).run())
 
 
 def run_phylogenomics(context: SimpleNamespace):
-    start_time = time()
-    out = UCEPhylogenomicsFacade(context).run()
-    tracking["Execution Time"] = strftime("%H:%M:%S", gmtime(time() - start_time))
-    tracking["FASTQ"] = context.contigs
-    tracking["Standard output"] = out
-    tracking["Commands"] = context.commands
-    save_tracking_file(context.log_dir + "/" + context.tracking_file, tracking)
+    save_tracking_file(PhylogenomicsFacade(context).run())
--- src/uceasy/tracking.py
+++ src/uceasy/tracking.py
@@ -5,19 +5,43 @@ such as system information and parameters used.
 """
 
 import csv
+import sys
 import platform
-from datetime import datetime
-from types import SimpleNamespace
+import getpass
 
 
-def save_tracking_file(path: str, context: SimpleNamespace):
-    first_row = ["Operating System", "Current Time"]
-    second_row = [platform.platform(), datetime.now()]
-    for k, v in context.__dict__.items():
+from uceasy.adapters import CommandResult
+
+
+tracking = {
+    "UCEasy command": sys.argv,
+    "Standard output": None,
+    "Execution time": None,
+    "Who ran": getpass.getuser(),
+    "Operating System": platform.platform(),
+    "Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+}
+
+
+def save_tracking_file(cmds: List[CommandResult]):
+    first_row = []
+    second_row = []
+    cmds_rows = []
+    for k, v in tracking.items():
         first_row.append(k)
         second_row.append(v)
+
+    for cmd in cmds:
+        row = []
+        row[0] = cmd.command
+        row[1] = cmd.stdout
+        row[2] = cmd.execution_time
+        row[3] = "UCEasy"
+        cmds_rows.append(row)
 
     with open(path, "w") as f:
         writer = csv.writer(f, delimiter=",")
         writer.writerow(first_row)
         writer.writerow(second_row)
+        for c in cmds_rows:
+            writer.writerow(c)
