8e696dd0ed19df0b8e8f8bad85449f56fc2eb842
Caio Raposo <caioraposo@protonmail.com>
2021-08-19 14:38:18 -0300

    Remove code coverage dependecies and nox session

--- noxfile.py
+++ noxfile.py
@@ -22,18 +22,10 @@ def install_with_constraints(session, *args, **kwargs):
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
-    args = session.posargs or ["--cov"]
+    args = session.posargs
     session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "pytest", "coverage[toml]", "pytest-cov")
+    install_with_constraints(session, "pytest")
     session.run("pytest", *args)
-
-
-@nox.session(python="3.8")
-def coverage(session):
-    """Upload coverage data."""
-    install_with_constraints(session, "coverage[toml]", "codecov")
-    session.run("coverage", "xml", "--fail-under=0")
-    session.run("codecov", *session.posargs)
 
 
 @nox.session(python=["3.8", "3.7"])
