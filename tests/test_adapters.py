a1c329ca9a4ded609c5cfcf67928937876e9f932
Caio Raposo <caioraposo@protonmail.com>
2021-08-19 15:32:15 -0300

    Update tests

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -15,14 +15,14 @@ def test_illumiprocessor_help(adapters):
 
 def test_trinity_help(adapters):
     cmd = adapters["trinity"](["--help"])
-    assert "usage: phyluce_assembly_assemblo_trinity" in cmd[0]
+    assert "usage: phyluce_assembly_assemblo_trinity" in cmd.stdout[0]
 
 
 def test_spades_help(adapters):
     cmd = adapters["spades"](["--help"])
-    assert "usage: phyluce_assembly_assemblo_spades" in cmd[0]
+    assert "usage: phyluce_assembly_assemblo_spades" in cmd.stdout[0]
 
 
 def test_match_contigs_to_probes_help(adapters):
     cmd = adapters["match_contigs_to_probes"](["--help"])
-    assert "usage: phyluce_assembly_match_contigs_to_probes" in cmd[0]
+    assert "usage: phyluce_assembly_match_contigs_to_probes" in cmd.stdout[0]
