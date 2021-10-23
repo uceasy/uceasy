657a4bbc2d794aa5cd60fcc94791586979ddf2c7
Caio Raposo <caioraposo@protonmail.com>
2021-10-23 13:56:21 -0300

    Don't implicitly save tracking file into log_dir

--- src/uceasy/run.py
+++ src/uceasy/run.py
@@ -5,15 +5,15 @@ from uceasy.tracking import save_tracking_file
 
 
 def run_quality_control(context: SimpleNamespace):
-    fl = context.log_dir + context.tracking_file
+    fl = context.tracking_file
     save_tracking_file(fl, QualityControlFacade(context).run())
 
 
 def run_assembly(context: SimpleNamespace):
-    fl = context.log_dir + context.tracking_file
+    fl = context.tracking_file
     save_tracking_file(fl, AssemblyFacade(context).run())
 
 
 def run_alignment(context: SimpleNamespace):
-    fl = context.log_dir + context.tracking_file
+    fl = context.tracking_file
     save_tracking_file(fl, UCEAlignmentFacade(context).run())
